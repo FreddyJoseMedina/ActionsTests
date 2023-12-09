b4cd42813b70732d508bf794912492d5a9fe1ee3
Freddy Medina <freddy.medina@super7ui.com>
2023-12-09 20:05:29 -0300

    last commit test

--- Database/Migration/mig2.sql
+++ Database/Migration/mig2.sql
@@ -1,9 +1,9 @@
-
+--liquibase formatted sql
 
 SET search_path TO wbtvd_owner, public;
 
---commentt: {FILENAME}
+--comment: {FILENAME}
 
---changesett {AUTHOR}:1_{ID} logicalFilePath:{migrations/seeders}/{FILENAME}
+--changeset {AUTHOR}:1_{ID} logicalFilePath:{migrations/seeders}/{FILENAME}
 
 --changeset {AUTHOR}:2_{ID} logicalFilePath:{migrations/seeders}/{FILENAME}
