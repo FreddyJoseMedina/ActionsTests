5339c1ce5ebae769c8d0231083d62e6a4cdf8371
Freddy Medina <freddy.medina@super7ui.com>
2023-12-09 18:31:48 -0300

    last commit test

--- Database/Seeders/seed1.sql
+++ Database/Seeders/seed1.sql
@@ -4,3 +4,8 @@ SET search_path TO wbtvd_owner, public;
 
 --comment: {FILENAME}
 
+--changeset {AUTHOR}:1_{ID} logicalFilePath:{migrations/seeders}/{FILENAME}
+
+--changeset {AUTHOR}:2_{ID} logicalFilePath:{migrations/seeders}/{FILENAME}
+
+--changeset {AUTHOR}:3_{ID} logicalFilePath:{migrations/seeders}/{FILENAME}
