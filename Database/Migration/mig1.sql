79509f0432937be3c8ef52e8080d9d11260a6f8b
Freddy Medina <freddy.medina@super7ui.com>
2023-12-11 10:23:51 -0300

    last commit test

--- Database/Migration/mig1.sql
+++ Database/Migration/mig1.sql
@@ -2,9 +2,9 @@
 
 SET search_path TO wbtvd_owner, public;
 
+--comment:20211216_114500_video_partners_endpoints.sql
 
--- 20211216_114500_video_partners_endpoints.sql
-
+--changeset {AUTHOR}:1_{ID}  logicalFilePath:{migrations/seeders}/{FILENAME}
 INSERT INTO wbtv_functions_permission (function_id, permission_name, sub_function_id, permission_type, display_order, is_default_selection, filter_id, uri_method, uri)
 VALUES (
            (
@@ -15,6 +15,7 @@ VALUES (
            'Update Video Partners', null, 3, 0, 0, null, 'PUT', '/asset/video/{videoId}/partner'
        ) ON CONFLICT DO NOTHING;
 
+--changeset {AUTHOR}:2_{ID}  logicalFilePath:{migrations/seeders}/{FILENAME}
 INSERT INTO wbtv_func_permission_role (role_id, function_permission_id, updated_by, updated_date, default_select)
 VALUES (
            (
